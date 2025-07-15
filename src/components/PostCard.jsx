e9e9651cf7c7704f9d55e21e182238b7f913dae4
Dhammikesl <dhammikerrppl@gmail.com>
2025-07-15 14:31:03 +0530

    update: admin view, delete post

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const PostCard = ({ title, content, href }) => {
+const PostCard = ({ title, content, href, children }) => {
     return (
         <div className='flex'>
             <div className='card max-w-sm max-h-sm p-2 rounded-sm gap-2 overflow-hidden bg-gray-400'>
+
                 <div className='card-body relative'>
+                    {children}
                     <div className='title text-lg uppercase font-semibold'>{title}</div>
                     <div className='content'>{content}</div>
                     <div className='text-xs mt-2 font-medium text-right'><a href={href} target="_blank" rel="noopener noreferrer">Read more</a></div>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,20 +5,60 @@ import BlogPost from './PostCard';
 const Posts = () => {
     const [posts, setPosts] = useState([]);
 
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    const ClickAdmin = () => {
+        setIsAdmin(!isAdmin);
+        // setIsAdmin(prev => !prev)
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:5000/posts')
-            .then(response => setPosts(response.data))
-            .catch(error => console.error("Error fetching projects:", error));
+        fetchPost();
     }, []);
+
+    const fetchPost = () => {
+        {
+            axios.get('http://localhost:5000/posts')
+                .then(response => setPosts(response.data))
+                .catch(error => console.error("Error fetching projects:", error));
+        }
+    }
+
+    const handleDelete = (id) => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this post?");
+        if (!confirmDelete) return;
+
+        axios
+            .delete(`http://localhost:5000/posts/${id}`)
+            .then(() => {
+                fetchPost(); // Refresh the list
+            })
+            .catch((error) => console.error('Error deleting project:', error));
+    };
 
     return (
         <div>
             <h2>Projects</h2>
+            <div>
+                <button
+                    className='button font-semibold text-fuchsia-50 p-2 bg-red-500 mb-5 float-right'
+                    onClick={ClickAdmin}
+                >
+                    {isAdmin ? 'Exit Admin' : 'Admin'}
+                </button>
+            </div>
+
             <div className='flex gap-2 flex-wrap'>
                 {posts.map(post => (
                     <div key={post.id}>
-                        {/* <strong>{post.title}</strong>: {post.content} */}
-                        <BlogPost title={post.title} content={post.content} href={post.href} />
+                        <BlogPost title={post.title} content={post.content} href={post.href}>
+                            {isAdmin && (
+                                <span className='flex gap-2'>
+                                    {/* <button className='bg-black py-2 px-3 mb-2 text-fuchsia-400'>Edit</button> */}
+                                    <button onClick={() => handleDelete(post.id)} className='bg-black py-2 px-3 mb-2 text-red-400'>Delete</button>
+                                </span>
+                            )}
+                        </BlogPost>
                     </div>
                 ))}
             </div>
